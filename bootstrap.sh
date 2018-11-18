057339b3f14ece708d4d40897f61cf6a8255cbc1
Mark Faga <mjfaga@gmail.com>
2018-11-18 10:57:03 -0500

    Fix simlinking of system files

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,21 +8,22 @@ function doIt() {
   # Install Homebrew
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+  # Copy system files
   for file in ./{bin,init,.?[a-z]*}; do
     localFile=${file:2}
     if [ "$localFile" != ".git" ] && [ "$localFile" != ".gitconfig" ] && [ "$localFile" != ".macos" ]; then
-      echo "Simlinking '$localFile'"
-      ln -s "$(pwd)/$localFile" "~/$localFile"
+      echo "Simlinking '$localFile'..."
+      ln -sf "$(pwd)/$localFile" ~/$localFile
     fi;
     unset localFile;
   done;
   unset file;
 
-  echo "Simlinking 'init.vim'"
-  ln -s "$(pwd)/init.vim" ~/.vimrc
-  ln -s "$(pwd)/init.vim" ~/.config/nvim/init.vim
+  echo "Simlinking 'init.vim'..."
+  ln -sf "$(pwd)/init.vim" ~/.vimrc
+  ln -sf "$(pwd)/init.vim" ~/.config/nvim/init.vim
 
-  echo "Sourcing ~/.bash_profile"
+  echo "Sourcing '~/.bash_profile'..."
   source ~/.bash_profile;
 
   echo "Don't forget to configure your git name/email!"
