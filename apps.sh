4dd793b53f5f6b12baf5b588e9144d6cebca191f
Mark Faga <mjfaga@gmail.com>
2019-10-30 17:52:33 -0400

    Add RVM & Plugged bash scripts to apps.sh

--- apps.sh
+++ apps.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# Check for Homebrew Installation
 if ! which brew > /dev/null; then
-  echo "Installing Homebrew"
+  echo "Installing Homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi;
 
@@ -20,6 +19,17 @@ if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   echo "Switch to using brew-installed bash as default shell..."
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
   chsh -s /usr/local/bin/bash;
+fi;
+
+if [ ! -d "$HOME/.rvm" ]; then
+  echo "Installing RVM..."
+  \curl -sSL https://get.rvm.io | bash -s stable
+fi;
+
+if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
+  echo "Installing Plugged..."
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi;
 
 # Remove outdated versions from the cellar.
