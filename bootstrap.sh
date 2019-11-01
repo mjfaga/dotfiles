09d0664dd57df8d84b776599a59b36887b1f703b
Mark Faga <mjfaga@gmail.com>
2019-11-01 00:35:22 -0400

    Support dotfiles-local file append/symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,59 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+# Script is dependent on being run from the root repo directory
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit;
 
 git pull origin master;
 
+dotfilesLocal="../dotfiles-local";
+
 function doIt() {
-  # Copy system files
+  files=();
   for file in ./{bin,init,.?[a-z]*}; do
     localFile=${file:2}
-    if [ "$localFile" != ".git" ] && [ "$localFile" != ".gitconfig" ]; then
-      echo "Simlinking '$localFile'..."
-      ln -sfn "$(pwd)/$localFile" ~/$localFile
+    if [ "$localFile" != ".git" ] &&
+      [ "$localFile" != ".gitignore" ]; then
+      fallbackOnSymlink=true
+
+      if [ -d "$dotfilesLocal" ]; then
+        if [ -f "$dotfilesLocal/$localFile" ]; then
+          files+=("$localFile")
+          if [ -h ~/$localFile ]; then
+            echo "unlinking $localFile"
+            unlink ~/$localFile
+          fi
+
+          echo "Copying '$localFile'..."
+          cp "$(pwd)/$localFile" ~/$localFile
+          echo "Appending '$dotfilesLocal/$localFile'..."
+          cat $dotfilesLocal/$localFile >> ~/$localFile
+          fallbackOnSymlink=false
+        fi
+      fi
+
+      if $fallbackOnSymlink; then
+        echo "Simlinking '$localFile'..."
+        ln -sfn "$(pwd)/$localFile" ~/$localFile
+      fi;
+
+      unset fallbackOnSymlink;
     fi;
     unset localFile;
   done;
+  if [ -d "$dotfilesLocal" ]; then
+    for file in $dotfilesLocal/.?[a-z]*; do
+      localFile=${file:2}
+      localFile=$(basename -- "$localFile")
+      if [ "$localFile" != ".git" ] &&
+        [ "$localFile" != ".gitignore" ]; then
+        if [[ ! " ${files[*]} " =~ $localFile ]]; then
+          echo "Simlinking '$localFile'..."
+          ln -sfn "$(pwd)/$dotfilesLocal/$localFile" ~/$localFile
+        fi;
+      fi;
+      unset localFile;
+    done;
+  fi;
   unset file;
 
   echo "Simlinking 'init.vim'..."
@@ -22,16 +62,9 @@ function doIt() {
 
   echo "Sourcing '~/.bash_profile'..."
   source ~/.bash_profile;
-
-  echo "Copying ..gitconfig'..."
-  cp .gitconfig ~/.gitconfig
-
-  echo "Don't forget to configure your git name/email:"
-  echo "git config --global user.name \"First Last\""
-  echo "git config --global user.email \"email@example.com\""
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
   doIt;
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
