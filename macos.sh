cbcbff2dff409592d5c6b52bf68e412f8656a1c6
Mark Faga <mjfaga@gmail.com>
2023-04-25 14:24:05 -0400

    feat: move screenshots directory to downloads

--- macos.sh
+++ macos.sh
@@ -200,10 +200,10 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Save screenshots to the custom folder
-if [ ! -d "$HOME/Documents/Screenshots" ]; then
-  mkdir ~/Documents/Screenshots
+if [ ! -d "$HOME/Downloads/Screenshots" ]; then
+  mkdir ~/Downloads/Screenshots
 fi
-defaults write com.apple.screencapture location -string "${HOME}/Documents/Screenshots"
+defaults write com.apple.screencapture location -string "${HOME}/Downloads/Screenshots"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
